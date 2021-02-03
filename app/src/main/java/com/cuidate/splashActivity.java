ac9c2df1827e80db105b9e7cc65a1350c2d3ffe5
yashkumarsharma96 <yash.kumar.sharma96@gmail.com>
2021-02-03 12:20:20 +0530

    Initial Commit

---
+++ app/src/main/java/com/cuidate/splashActivity.java
@@ -0,0 +1,40 @@
+package com.cuidate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class splashActivity extends AppCompatActivity {
+
+    private PrefManager prefManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Context splashScreenContext=getApplicationContext();
+        prefManager = new PrefManager(this);
+        int delay=4000;
+        setContentView(R.layout.activity_splash);
+        if (!prefManager.isFirstTimeLaunch()) {
+                delay=1000;
+        }
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+            public void run() {
+                Intent mainIntent = new Intent(splashScreenContext, WelcomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, delay);
+
+    }
+}
